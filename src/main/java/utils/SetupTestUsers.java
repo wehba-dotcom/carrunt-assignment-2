e8c94fda3dee76a4d6b818cae05cf21760b36fb6
wehba <Lenovo@wehba.com>
2021-11-10 11:56:39 +0100

    make all requestes Parallel from project to others APIs

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -23,7 +23,6 @@ public class SetupTestUsers {
     User user = new User("user", "test");
     User admin = new User("admin", "test");
     User both = new User("user_admin", "test");
-
     
     if(admin.getUserPass().equals("test")||user.getUserPass().equals("test")||both.getUserPass().equals("test"))
       throw new UnsupportedOperationException("You have not changed the passwords");
